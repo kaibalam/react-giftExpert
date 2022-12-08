31c7d8125f988f9d3ec145675d66fe520f7b4c60
RICARDO VALENZUELA <ricardo.valenzuela@ericsson.com>
2022-12-07 23:14:31 -0600

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['']);
+
+  const onAddCategory = (newCategory) => {
+    //Valorant
+    // console.log(newCategory);
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+    // setCategories( cat => [...cat, 'Valorant']);
+
+  }
+
+  return (
+    <>
+            <h1>GifExpertApp</h1>
+
+      
+      <AddCategory
+        onNewCategory={(value) => onAddCategory(value)}
+      />
+
+        {categories.map(category => {
+          return (
+            <GifGrid
+              key={category}
+              category={category} />
+          )
+        })}
+      
+     
+    </>
+  )
+}
+
+
